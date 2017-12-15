f7bd2dd95ee21344691eb48115783ed105e2a79c
Ross Schlaikjer <ross@schlaikjer.net>
2017-12-15 21:30:02 +0000

    Add rebar stuff

---
+++ src/minidump_app.erl
@@ -0,0 +1,16 @@
+-module(minidump_app).
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    {ok, self()}.
+
+stop(_State) ->
+    ok.
+
